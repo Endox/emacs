914c2e4e1978198fb8ce930ec2d8e1e4a0ba96a1
Michal Varga <Michal Varga>
2016-01-21 21:54:54 +0100

    Helm, powerline, C-tab, theme

--- init.el
+++ init.el
@@ -1,6 +1,31 @@
-;; Set custom theme path
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("b04425cc726711a6c91e8ebc20cf5a3927160681941e06bc7900a5a5bfe1a77f" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "257ce19f15fa5e08260f269b1ea5740c299c601d5e33d2f6066ddb2dae262c8c" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 
+
+;; Set custom theme path
 (set-default-font "Liberation Mono")
+(require 'package)
+(add-to-list
+ 'package-archives
+ '("melpa" . "http://melpa.org/packages/")
+ t)
+
+(package-initialize)
+(helm-mode 1)
+(global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
 
 ;; Set C style
 (add-hook 'c++-mode-common-hook '(lambda () (setq c-default-style "linux"
@@ -18,3 +43,20 @@
 (scroll-bar-mode 0)
 (tool-bar-mode 0)
 (menu-bar-mode 0)
+
+(load-theme 'ujelly)
+
+;;(setq powerline-arrow-shape 'curve)
+;;(setq powerline-default-separator-dir '(right . left))
+;;(setq sml/theme 'powerline)
+;;(sml/setup)
+
+(powerline-default-theme)
+(set-face-attribute 'mode-line nil
+		    :foreground "Black"
+		    :background "#99ad6a"
+		    :box nil)
+(setq powerline-arrow-shape 'curve)
+(setq powerline-default-separator 'slant)
+
+(global-set-key [C-tab] 'other-window)
